46025a344696616b46b29271332cdbb1679727b2
asafzaf <asaf.zafrir@gmail.com>
2024-04-09 02:16:36 +0300

    fixed login func

--- src/HTTP/http.jsx
+++ src/HTTP/http.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://war-news.onrender.com";
+const baseURL = "https://volunteer-coordinator.onrender.com";
 
 export const http = axios.create({
   baseURL: baseURL,
@@ -8,6 +8,20 @@ export const http = axios.create({
     "Content-type": "application/json",
   },
 });
+
+const loginUser = async (email, password) => {
+  console.log({ email: email, password: password });
+  try {
+    const response = await http.post("/api/v1/users/login", {
+      email: email,
+      password: password,
+    });
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("An error occurred while fetching data:", error);
+  }
+};
 
 const getAllItems = async () => {
   try {
@@ -55,4 +69,4 @@ const deleteItem = async (id) => {
   }
 };
 
-export { getAllItems, getItem, createItem, updateItem, deleteItem };
+export { getAllItems, getItem, createItem, updateItem, deleteItem, loginUser };
