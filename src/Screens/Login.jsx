a2091007ce55329284bde86c6162dff2f88a881e
asafzaf <asaf.zafrir@gmail.com>
2024-04-09 02:16:44 +0300

    fixed login page

--- src/Screens/Login.jsx
+++ src/Screens/Login.jsx
@@ -1,12 +1,47 @@
+import { useContext, useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { Box } from "@mui/material";
 import Header from "../Components/Header.jsx";
 import Footer from "../Components/Footer.jsx";
-import { Link } from "react-router-dom";
+import { loginUser } from "../HTTP/http.jsx";
 import "../style/styles.css";
 
-const Login = () => {
+import { AuthContext } from "../store/auth-context.jsx";
+
+const Login = (props) => {
+  const [email, setMail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // const [isAuthenticating, setIsAuthenticating] = useContext(false);
+
+  const authCtx = useContext(AuthContext);
+  async function loginHandler(email, password) {
+    try {
+      // setIsAuthenticating(true);
+      const response = await loginUser(email, password);
+      authCtx.authenticate(
+        response.token,
+        response.data.user._id,
+        response.data.user.name
+      );
+      window.localStorage.setItem("token", JSON.stringify(response.token));
+      window.localStorage.setItem("id", JSON.stringify(response.data.user._id));
+      window.localStorage.setItem("name", JSON.stringify(response.data.user.name));
+      console.log(authCtx.token, authCtx.userId, authCtx.userName);
+      window.location.href = "/";
+    } catch (error) {
+      console.log(error);
+      //add modal
+      // setIsAuthenticating(false);
+    }
+  }
+
+  const moveToSignUp = () => {
+    /* eslint-disable react/prop-types */
+    props.setSignUp(true);
+  };
+
   return (
     <Box className="root">
       <Box className="container-primary">
@@ -15,12 +50,18 @@ const Login = () => {
           <form className="flex-container-col">
             <h1 className="title">Login</h1>
             <TextField
+              onChange={(e) => {
+                setMail(e.target.value);
+              }}
               className="input-field"
               id="outlined-basic"
               label="Username"
               variant="outlined"
             />
             <TextField
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
               className="input-field"
               id="filled-password-input"
               label="Password"
@@ -29,12 +70,23 @@ const Login = () => {
               variant="outlined"
             />
             <div className="btn-wrapper">
-              <Button type="submit" variant="contained" className="nav-btn">
+              <Button
+                variant="contained"
+                className="nav-btn"
+                onClick={() => 
+                  loginHandler(email, password)
+                }
+              >
                 Sign-in
               </Button>
             </div>
           </form>
           {/* <Link to="/signup">Sign-Up</Link> */}
+          <Button
+            variant="contained"
+            className="nav-btn"
+            onClick={moveToSignUp}
+          ></Button>
         </Box>
         <Footer></Footer>
       </Box>
