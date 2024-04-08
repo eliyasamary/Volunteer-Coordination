b93e04b644fe4a00251978b13b5b4b99f0f90b7d
asafzaf <asaf.zafrir@gmail.com>
2024-04-09 02:15:57 +0300

    for login & signup (when there is nno token)

--- src/AuthContex.jsx
+++ src/AuthContex.jsx
@@ -1,18 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { useState } from "react";
 import "./App.css";
 import Box from "@mui/material/Box";
 import Header from "./Components/Header.jsx";
-import Navigation from "./Components/Navigation.jsx";
 import Footer from "./Components/Footer.jsx";
+import Login from "./Screens/Login";
+import NewItem from "./Screens/NewItem";
 
 const AuthContex = () => {
+  const [signUp, setSignUp] = useState(false);
+
   return (
     <Box className="root">
       <Box className="container-primary">
         <Header></Header>
-        <Navigation></Navigation>
       </Box>
-      <Outlet />
+      {signUp ? (
+        <NewItem setSignUp={setSignUp} />
+      ) : (
+        <Login setSignUp={setSignUp} />
+      )}
       <Footer></Footer>
     </Box>
   );
