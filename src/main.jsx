0864d9ec3894e7e48169f0acf84c18f6f64022ff
asafzaf <asaf.zafrir@gmail.com>
2024-04-09 02:16:10 +0300

    can delete

--- src/main.jsx
+++ src/main.jsx
@@ -1,57 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import AllItems from "./Screens/AllItems";
-import FindItem from "./Screens/FindItem";
-// import NewItem from "./Screens/NewItem";
-import Dashboard from "./Screens/Dashboard";
-import ItemPage from "./Screens/ItemPage";
+import App from "./App.jsx";
+
 // import ErrorNotFound from "./Components/ErrorNotFound404.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    // errorElement: <ErrorNotFound />,
-    children: [
-      {
-        path: "/allItems",
-        element: <AllItems />,
-        exact: true,
-      },
-      {
-        path: "/",
-        element: <AllItems />,
-        exact: true,
-      },
-      {
-        path: "/findItem",
-        element: <FindItem />,
-        exact: true,
-      },
-      {
-        path: "/dashboard",
-        element: <Dashboard />,
-        exact: true,
-      },
-      {
-        path: "/ItemPage/:itemId",
-        element: <ItemPage />,
-        exact: true,
-      },
-      // {
-      //   path: "/signup",
-      //   element: <NewItem />,
-      //   exact: true,
-      // },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
