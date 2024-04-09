a99cb83a794891868d35ee3f507fa5677223a684
Eliya <eliysamaea@gmail.com>
2024-04-09 16:00:23 +0300

    changing class names in order to be clearer

--- src/Screens/Dashboard.jsx
+++ src/Screens/Dashboard.jsx
@@ -9,19 +9,19 @@ const Dashboard = () => {
         <div className="card">
           <h2 className="detail-text">My Tasks</h2>
           <div className="card-items">
-            <p className="font-primary pink">Patient Care</p>
+            <p className="font-primary pink-container">Patient Care</p>
           </div>
         </div>
         <div className="card">
           <h2 className="detail-text">Tasks I Performed</h2>
           <div className="card-items">
-            <p className="font-primary green">hghghg</p>
+            <p className="font-primary green-container">hghghg</p>
           </div>
         </div>
         <div className="card">
           <h2 className="detail-text">Suggested Tasks</h2>
           <div className="card-items">
-            <p className="font-primary orange">hghghg</p>
+            <p className="font-primary orange-container">hghghg</p>
           </div>
         </div>
       </div>
