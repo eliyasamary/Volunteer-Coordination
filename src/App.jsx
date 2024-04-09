21228d2046ad54bc54419d8858b9632625198564
Eliya <eliysamaea@gmail.com>
2024-04-09 20:10:51 +0300

    removing unnecessary comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-// import { Outlet } from "react-router-dom";
 import "./App.css";
-// import Box from "@mui/material/Box";
-// import Header from "./Components/Header.jsx";
-// import Navigation from "./Components/Navigation.jsx";
-// import Footer from "./Components/Footer.jsx";
 import AuthContextProvider from "./store/auth-context";
-// import { useContext } from "react";
 import AuthContex from "./AuthContex.jsx";
 import Router from "./Router.jsx";
 
 const App = () => {
-  // const name = localStorage.getItem("name");
-
   return (
     <>
       <AuthContextProvider>
