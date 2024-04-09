073e9660ffe4cf1918ff6a7c5bacebe53451bde9
Eliya <eliysamaea@gmail.com>
2024-04-09 19:15:16 +0300

    removing unnecessary consol logs

--- src/HTTP/http.jsx
+++ src/HTTP/http.jsx
@@ -10,63 +10,71 @@ export const http = axios.create({
 });
 
 const loginUser = async (email, password) => {
-  console.log({ email: email, password: password });
   try {
     const response = await http.post("/api/v1/users/login", {
       email: email,
       password: password,
     });
-    console.log(response.data);
     return response.data;
   } catch (error) {
-    console.error("An error occurred while fetching data:", error);
+    console.error("An error occurred while fetching data / signin", error);
   }
 };
 
-const getAllItems = async () => {
+const signupUser = async (data) => {
   try {
-    const response = await http.get("/news");
-    return response.data.news;
-  } catch (error) {
-    console.error("An error occurred while fetching data:", error);
-  }
-};
-
-const getItem = async (id) => {
-  try {
-    const response = await http.get("/news/" + id);
+    const response = await http.post("/api/v1/users/signup", data);
     return response.data;
   } catch (error) {
-    console.error("An error occurred while fetching data:", error);
+    console.error("An error occurred while fetching data / signup", error);
   }
 };
 
-const createItem = async (data) => {
-  try {
-    const response = await http.post("/news", data);
-    return response.data;
-  } catch (error) {
-    console.error("An error occurred while fetching data:", error);
-  }
-};
+// Still not complete
+// const getAllItems = async () => {
+//   try {
+//     const response = await http.get("/news");
+//     return response.data.news;
+//   } catch (error) {
+//     console.error("An error occurred while fetching data:", error);
+//   }
+// };
 
-const updateItem = async (data) => {
-  try {
-    console.log(data);
-    const response = await http.put("/news/" + data._id, data);
-    return response.data;
-  } catch (error) {
-    console.error("An error occurred while fetching data:", error);
-  }
-};
+// const getItem = async (id) => {
+//   try {
+//     const response = await http.get("/news/" + id);
+//     return response.data;
+//   } catch (error) {
+//     console.error("An error occurred while fetching data:", error);
+//   }
+// };
 
-const deleteItem = async (id) => {
-  try {
-    const response = await http.delete("/news/" + id);
-    return response.data;
-  } catch (error) {
-    console.error("An error occurred while fetching data:", error);
-  }
-};
+// const createItem = async (data) => {
+//   try {
+//     const response = await http.post("/news", data);
+//     return response.data;
+//   } catch (error) {
+//     console.error("An error occurred while fetching data:", error);
+//   }
+// };
 
-export { getAllItems, getItem, createItem, updateItem, deleteItem, loginUser };
+// const updateItem = async (data) => {
+//   try {
+//     console.log(data);
+//     const response = await http.put("/news/" + data._id, data);
+//     return response.data;
+//   } catch (error) {
+//     console.error("An error occurred while fetching data:", error);
+//   }
+// };
+
+// const deleteItem = async (id) => {
+//   try {
+//     const response = await http.delete("/news/" + id);
+//     return response.data;
+//   } catch (error) {
+//     console.error("An error occurred while fetching data:", error);
+//   }
+// };
+
+export { loginUser, signupUser };
