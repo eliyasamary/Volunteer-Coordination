2fc3c42f0c2e6f4d17c4b639d82e5600a095b240
Eliya <eliysamaea@gmail.com>
2024-04-09 16:00:56 +0300

    adding a messages page / not final

---
+++ src/Screens/Messages.jsx
@@ -0,0 +1,12 @@
+import "../style/styles.css";
+import Box from "@mui/material/Box";
+
+const Messages = () => {
+  return (
+    <Box className="content-box">
+      <h1 className="title">Messages</h1>
+    </Box>
+  );
+};
+
+export default Messages;
