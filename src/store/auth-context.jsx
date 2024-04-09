b7619953c42b80448241d2a3ab433636b9ea789c
Eliya <eliysamaea@gmail.com>
2024-04-09 16:02:12 +0300

    styling changes

--- src/store/auth-context.jsx
+++ src/store/auth-context.jsx
@@ -1,44 +1,42 @@
 import { createContext, useState } from "react";
 
 export const AuthContext = createContext({
-    token: "",
-    userId: "",
-    userName: "",
-    isAuthenticated: false,
-    // eslint-disable-next-line no-unused-vars
-    authenticate: (token) => {},
-    logout: () => {},
-  });
-  /* eslint-disable react/prop-types */
-
+  token: "",
+  userId: "",
+  userName: "",
+  isAuthenticated: false,
+  // eslint-disable-next-line no-unused-vars
+  authenticate: (token) => {},
+  logout: () => {},
+});
+/* eslint-disable react/prop-types */
 
 function AuthContextProvider({ children }) {
-    const [authToken, setAuthToken] = useState();
-    const [userId, setUserId] = useState();
-    const [userName, setUserName] = useState();
-  
-    function authenticate(token, id, name) {
-      setAuthToken(token);
-      setUserId(id);
-      setUserName(name);
-    }
-  
-    function logout() {
-      setAuthToken(null);
-      setUserId(null);
-      setUserName(null);
-    }
-  
-    const value = {
-      token: authToken,
-      userId: userId,
-      userName: userName,
-      isAuthenticated: !!authToken,
-      authenticate: authenticate,
-      logout: logout,
-    };
-    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+  const [authToken, setAuthToken] = useState();
+  const [userId, setUserId] = useState();
+  const [userName, setUserName] = useState();
+
+  function authenticate(token, id, name) {
+    setAuthToken(token);
+    setUserId(id);
+    setUserName(name);
   }
-  
-  export default AuthContextProvider;
-  
+
+  function logout() {
+    setAuthToken(null);
+    setUserId(null);
+    setUserName(null);
+  }
+
+  const value = {
+    token: authToken,
+    userId: userId,
+    userName: userName,
+    isAuthenticated: !!authToken,
+    authenticate: authenticate,
+    logout: logout,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export default AuthContextProvider;
