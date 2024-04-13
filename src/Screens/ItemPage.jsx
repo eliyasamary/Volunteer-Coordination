1572d8bc25e283cc7b79087b98035f79721fa2b0
Eliya <eliysamaea@gmail.com>
2024-04-13 13:21:28 +0300

    change to a real data from DB

--- src/Screens/ItemPage.jsx
+++ src/Screens/ItemPage.jsx
@@ -1,22 +1,49 @@
 import "../style/styles.css";
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
-import DummyVolunteeringTasks from "../DummyData/DummyVolunteeringTasks.json";
 import Button from "@mui/material/Button";
+import { getTask } from "../HTTP/http";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const ItemPage = () => {
+  const [item, setItem] = useState();
+  const [loading, setLoading] = useState(true);
   const { itemId } = useParams();
-  const data = DummyVolunteeringTasks.find((item) => item._id === itemId);
+
+  useEffect(() => {
+    const fetchTask = async () => {
+      try {
+        setLoading(true);
+        const response = await getTask(itemId);
+        setItem(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching task " + itemId, error);
+        setLoading(false);
+      }
+    };
+
+    fetchTask();
+  }, [itemId]);
+
+  if (loading) {
+    return (
+      <Box className="loading">
+        <CircularProgress color="inherit" />
+      </Box>
+    );
+  }
   return (
     <Box className="content-box">
-      <h1 className="title">{data.title}</h1>
+      <h1 className="title">{item.title}</h1>
       <Box className="items-container center-flex-col ">
         <Box className="detail-box ">
           <Box className="flex-container-row">
             <Box className="detail-title">
               <h3>Description</h3>
             </Box>
-            <Box className="detail-text">{data.description}</Box>
+            <Box className="detail-text">{item.description}</Box>
           </Box>
         </Box>
         <Box className="detail-box ">
@@ -24,7 +51,7 @@ const ItemPage = () => {
             <Box className="detail-title">
               <h3>Skills</h3>
             </Box>
-            <Box className="detail-text">{data.skills}</Box>
+            <Box className="detail-text">{item.skills}</Box>
           </Box>
         </Box>
         <Box className="detail-box ">
@@ -32,7 +59,7 @@ const ItemPage = () => {
             <Box className="detail-title">
               <h3>Requirements</h3>
             </Box>
-            <Box className="detail-text">{data.requirements}</Box>
+            <Box className="detail-text">{item.requirements}</Box>
           </Box>
         </Box>
         <Box className="detail-box ">
@@ -40,7 +67,7 @@ const ItemPage = () => {
             <Box className="detail-title">
               <h3>Loction</h3>
             </Box>
-            <Box className="detail-text"> {data.location}</Box>
+            <Box className="detail-text"> {item.location}</Box>
           </Box>
         </Box>
         <Box className="detail-box ">
@@ -48,7 +75,7 @@ const ItemPage = () => {
             <Box className="detail-title">
               <h3>Time</h3>
             </Box>
-            <Box className="detail-text">{data.time}</Box>
+            <Box className="detail-text">{item.time}</Box>
           </Box>
         </Box>
       </Box>
