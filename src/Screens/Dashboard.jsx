9d72dc797881bfa776204026bba16b807ea8fbbc
Eliya <eliysamaea@gmail.com>
2024-04-18 13:47:36 +0300

    in comments for now

--- src/Screens/Dashboard.jsx
+++ src/Screens/Dashboard.jsx
@@ -1,7 +1,36 @@
 import "../style/styles.css";
+import { useEffect, useState } from "react";
+import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
+import { getAllTasks } from "../HTTP/http";
 
 const Dashboard = () => {
+  const [myTasks, setMyTasks] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        setLoading(true);
+        const response = await getAllTasks();
+        setMyTasks(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching tasks:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchTasks();
+  }, []);
+
+  if (loading) {
+    return (
+      <Box className="loading">
+        <CircularProgress color="inherit" />
+      </Box>
+    );
+  }
   return (
     <Box className="content-box">
       <h1 className="title">Dashboard</h1>
@@ -9,7 +38,17 @@ const Dashboard = () => {
         <div className="card">
           <h2 className="detail-text">My Tasks</h2>
           <div className="card-items">
-            <p className="font-primary pink-container">Patient Care</p>
+            {myTasks.map((item) => (
+              // <Link
+              //   to={`/ItemPage/${item._id}`}
+              //   key={item._id}
+              //   className="link-decoration"
+              // >
+              <p key={item._id} className="font-primary pink-container">
+                {item.title}
+              </p>
+              // </Link>
+            ))}
           </div>
         </div>
         <div className="card">
