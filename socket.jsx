62f606e8af1cd9e490eb0e371cecef7e4e881ab7
Eliya <eliysamaea@gmail.com>
2024-04-23 17:54:10 +0300

    cahnge the loalhost

--- socket.jsx
+++ socket.jsx
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // eslint-disable-next-line react-refresh/only-export-components
-const URL = "http://localhost:3000";
+const URL = "http://localhost:5173";
 
 export const socket = io(URL, {
   query: { user_id: window.localStorage.getItem("id") },
