4deeea8b034973a17a08abb799e7b6c16efd99cb
Eliya <eliysamaea@gmail.com>
2024-04-23 18:40:44 +0300

    change from localhost to the app url

--- socket.jsx
+++ socket.jsx
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // eslint-disable-next-line react-refresh/only-export-components
-const URL = "http://localhost:5173";
+const URL = "https://volunteer-coordination.netlify.app/";
 
 export const socket = io(URL, {
   query: { user_id: window.localStorage.getItem("id") },
