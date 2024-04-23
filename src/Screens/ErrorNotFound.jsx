50294bcbeaf54e6a5f773ac1e309e7500dabb375
Eliya <eliysamaea@gmail.com>
2024-04-23 20:32:54 +0300

    updating route

--- src/Screens/ErrorNotFound.jsx
+++ src/Screens/ErrorNotFound.jsx
@@ -1,12 +1,12 @@
 import Box from "@mui/material/Box";
 // import "../CSS/style.css";
-import notFoundImg from "../assets/404NotFound.png";
+// import notFoundImg from "../assets/404NotFound.png";
 
 const Error = () => {
   return (
     <Box className="flex-container-col-secondary">
       <h1 className="title-primary">Oops!</h1>
-      <img src={notFoundImg} alt="404NotFound" className="not-found-img" />
+      {/* <img src={notFoundImg} alt="404NotFound" className="not-found-img" /> */}
 
       <h2 className="text-primary">
         Seems like you have taken a wrong turn ...
